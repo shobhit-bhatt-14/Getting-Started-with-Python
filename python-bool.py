13861050133bfd0054edf79a86a0133c6c952343
SHOBHIT BHATT <shobhit.bhatt.14@gmail.com>
2021-06-07 23:28:19 +0530

    update

---
+++ python-bool.py
@@ -0,0 +1,15 @@
+a = bool('True')
+b = bool('False')
+c = bool('')
+d = bool(' ')
+e = bool('hello')
+f = bool(0)
+g = bool(1)
+h = bool('1')
+
+print(a,b,c,d,e,f,g,h,sep=' ')
+
+#auto bool
+print(1>3)
+print(1==2)
+print(3>0)
